9d198f6f5f7132c7205d0d02883f29534b26b786
MANISH-GIRI <manish.giri.me@gmail.com>
2017-07-09 19:51:08 -0400

    Use an anonymous inner class, eliminating the need of a new class to implement the interface

--- src/lambdas/lambdapractice/Greeter.java
+++ src/lambdas/lambdapractice/Greeter.java
@@ -17,9 +17,23 @@ public class Greeter {
 
         Greeter greeter = new Greeter();
 
-        // Create an instance of HelloWorldGreeting, which implements Greeting interface
+        // Method 1 - Create an instance of HelloWorldGreeting, which implements Greeting interface
         HelloWorldGreeting helloWorldGreeting = new HelloWorldGreeting();
         greeter.greet(helloWorldGreeting);
+
+        // Method 2 - Create an anonymous class that implements the interface (eliminating need for a new class)
+        Greeting innerClassGreeting = new Greeting() {
+            @Override
+            public void perform() {
+                System.out.println("Hello World from Anonymous class.");
+            }
+        };
+        greeter.greet(innerClassGreeting);
+
+
+
     }
+
+
 
 }
